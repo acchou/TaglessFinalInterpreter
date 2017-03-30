dd2843ce2ef13cbb53a9b37b9023c6e0dba8dadf
Andy Chou <acchou2@gmail.com>
2017-03-29 17:39:36 -0700

    Add initial embedding from p3.

--- TaglessFinalInterpreter/main.swift
+++ TaglessFinalInterpreter/main.swift
@@ -2,11 +2,20 @@
 //  main.swift
 //  TaglessFinalInterpreter
 //
+//  Examples from this paper: http://okmij.org/ftp/tagless-final/course/lecture.pdf
+//
 //  Created by Andy Chou on 3/29/17.
 //  Copyright © 2017 Andy Chou. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+// Page 3
+indirect enum Exp {
+    case Lit(Int)
+    case Neg(Exp)
+    case Add(Exp, Exp)
+}
+
+let ti1: Exp = .Add(.Lit(8), .Neg(.Add (.Lit(1), .Lit(2))))
 
